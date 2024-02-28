4b629cc4fb268ad0c69343ef768247cc674590f7
KamalSoltanaliyev <kamalas@code.edu.az>
2024-02-28 21:13:46 +0400

    first commit

---
+++ FrontToBack/Contexts/ProniaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FrontToBack.Models;
+
+namespace FrontToBack.Contexts
+{
+    public class ProniaDbContext : DbContext
+    {
+        public ProniaDbContext(DbContextOptions<ProniaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Slider>? Sliders { get; set; }
+        public DbSet<Shipping>? Shippings { get; set; }
+    }
+}
